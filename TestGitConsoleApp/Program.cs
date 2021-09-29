61a332ed4b77801f02625973fa7b19f9c33f4b91
daniil.karas <daniil.karas@nure.ua>
2021-09-29 19:37:08 +0300

    test change (v2)

--- TestGitConsoleApp/Program.cs
+++ TestGitConsoleApp/Program.cs
@@ -6,7 +6,7 @@ namespace TestGitConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! v2");
         }
     }
 }
